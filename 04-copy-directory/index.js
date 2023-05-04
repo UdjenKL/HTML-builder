3665458ae102ce50c80456087a22e3a6a3e032e6
Yauhen Klimovich <webrubin1987@gmail.com>
2023-05-04 20:59:05 +0300

    feat: finish 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+// подключаем модуль fs и используем промисифицированные функции из раздела "Promise API".
+const fs = require('fs').promises;
 
+// подключаем модуль path для работы с путями к файлам и папкам.
+const path = require('path');
+
+// функция принимает 2 аргумента (исходную папку и целевую папку)
+async function copyDir(sourceDir, targetDir) {
+  try {
+    // создаем папку files-copy, если ее нет
+    await fs.mkdir(targetDir, { recursive: true });
+
+    // получаем список файлов в исходной папке
+    const files = await fs.readdir(sourceDir);
+
+    // копируем каждый файл в целевую папку
+    for (const file of files) {
+      const sourceFile = path.join(sourceDir, file);
+      const targetFile = path.join(targetDir, file);
+      await fs.copyFile(sourceFile, targetFile);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// пример использования
+copyDir('files', 'files-copy');
